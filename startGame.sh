16632d85d199e6f282309e028bd06f72a6f474eb
Chris L8 <ekpyroticfrood@gmail.com>
2019-02-27 20:10:20 -0600

    Clean up Startup script. Use npm ci. Update node when updating depenencies.

--- startGame.sh
+++ startGame.sh
@@ -3,22 +3,22 @@
 # Grab and save the path to this script
 # http://stackoverflow.com/a/246128
 SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+while [[ -h "$SOURCE" ]]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
   SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  [[ ${SOURCE} != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
-SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-# echo ${SCRIPTDIR} # For debugging
+SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+# echo ${SCRIPT_DIR} # For debugging
 
-if [ -d ${HOME}/.nvm ]; then
+if [[ -d ${HOME}/.nvm ]]; then
   export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  [[ -s "$NVM_DIR/nvm.sh" ]] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [[ -s "$NVM_DIR/bash_completion" ]] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 fi
 
 which node > /dev/null
-if [ ! $? -eq 0 ]
+if [[ ! $? -eq 0 ]]
 then
     echo "You must install Node.js to run this."
     echo "See https://nodejs.org/"
@@ -27,37 +27,40 @@ fi
 
 while test $# -gt 0
 do
-        case "$1" in
-                --service) RUN_AS_SERVICE=true
-                ;;
-                --update) UPDATE_MODULES=true
-                ;;
-        *) echo "Invalid argument"
-        exit
+    case "$1" in
+        --service) RUN_AS_SERVICE=true
         ;;
-        esac
-        shift
+        --update) UPDATE_MODULES=true
+        ;;
+    *) echo "Invalid argument"
+    exit
+    ;;
+    esac
+    shift
 done
 
-cd ${SCRIPTDIR}/Station
+cd ${SCRIPT_DIR}/Station
 
-if [ ! -d "node_modules" ]; then
-  npm install
+if [[ ! -d "node_modules" ]]; then
+  npm ci
 else
   if test "${UPDATE_MODULES}" == "true"; then
-    npm update
+    if [[ -d ${HOME}/.nvm ]]; then
+      nvm install --lts
+    fi
+    npm ci
   fi
 fi
 
 if test "${RUN_AS_SERVICE}" == "true"; then
-  ${SCRIPTDIR}/Station/node_modules/pm2/bin/pm2 start ${SCRIPTDIR}/Station/pm2Config.json
+  ${SCRIPT_DIR}/Station/node_modules/pm2/bin/pm2 start ${SCRIPT_DIR}/Station/pm2Config.json
   echo "To see the logs run:"
-  echo "${SCRIPTDIR}/Station/node_modules/pm2/bin/pm2 logs"
+  echo "${SCRIPT_DIR}/Station/node_modules/pm2/bin/pm2 logs"
   echo "To stop the program run:"
-  echo "${SCRIPTDIR}/Station/node_modules/pm2/bin/pm2 stop Game"
+  echo "${SCRIPT_DIR}/Station/node_modules/pm2/bin/pm2 stop Game"
   echo "To restart the game (with new code for example) run:"
-  echo "${SCRIPTDIR}/Station/node_modules/pm2/bin/pm2 restart Game"
+  echo "${SCRIPT_DIR}/Station/node_modules/pm2/bin/pm2 restart Game"
 else
   node index.js
 fi
-cd ${SCRIPTDIR}
+cd ${SCRIPT_DIR}
